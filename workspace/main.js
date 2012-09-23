ff88b5fe02401993e6f1c66c311c30ec3ba083f7
Michal Srb <ms6611@imperial.ac.uk>
2012-09-24 01:21:50 +0200

    Created a simplified workspace, without prototype dependency

--- workspace/main.js
+++ workspace/main.js
@@ -6,5 +6,33 @@ require({
     'coffee-script': '../lib/cs/coffee-script-iced'
   }
 },
-// ['cs!source/editor']
+ ['../bin/bs-js-compiler',
+ '../bin/bs-ometa-compiler',
+ '../bin/bs-ometa-optimizer',
+ '../bin/bs-ometa-js-compiler'],
+ function (jsc, oc, oo, BSOmetaJSCompiler) {
+
+  $("#doIt").click(function (e){
+    var sourceCode = $("#source").val();    
+    try {
+    var tree = BSOmetaJSCompiler.BSOMetaJSParser.matchAll(
+      sourceCode, "topLevel", undefined, function(m, i) {
+        alert("Error at m: " + m + " i: " + i);
+        throw new Error("Error at m: " + m + " i: " + i);
+      }
+    );
+    var result = BSOmetaJSCompiler.BSOMetaJSTranslator.match(
+      tree, "trans", undefined, function(m, i) {
+      alert("Translation error - please tell Alex about this!"); 
+        throw new Error("fail");
+      }
+    );   
+    } catch (e) {
+      console.log(e);
+      $("#result").text(e.toString());
+    }
+    
+    $("#result").text(result);
+  });
+ }
 );
