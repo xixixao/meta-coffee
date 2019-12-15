dd3dce7926fc0a097ab552ed6cf8969fdb452563
xixixao <xixixao@seznam.cz>
2019-12-14 21:30:39 -0800

    Fix links on website

--- _next/static/Aw5U0F_RzcPpxRbJcfFKB/pages/index.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{RNiq:function(n,i,o){"use strict";o.r(i);var t=o("q1tI"),u=o.n(t),e=o("F1wk"),l=u.a.createElement;i.default=function(){return l(e.a,null,l("div",null,"Hello World"))}},vlRD:function(n,i,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return o("RNiq")}])}},[["vlRD",0,1]]]);
